fb65e21dc32d8928e4c2bc881ec4f0a3ecd00be2
ChrisMwanya <cmwanya@gmail.com>
2022-06-17 17:16:20 +0100

    feat: add form

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -6,21 +7,25 @@ import ReCAPTCHA from "react-google-recaptcha";
 export default function Home() {
   const [email, setEmail] = React.useState("");
   const recaptchaRef = React.createRef();
-
-  <form onSubmit={handleSubmit}>
-    <ReCAPTCHA
-      ref={recaptchaRef}
-      size="normal"
-      sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SECRET}
-      onChange={onReCAPTCHAChange}
-    />
-    <input
-      onChange={handleChange}
-      required
-      type="email"
-      name="email"
-      placeholder="Email"
-    />
-    <button type="submit">Register</button>
-  </form>;
+  const handleSubmit = () => {};
+  const onReCAPTCHAChange = () => {};
+  const handleChange = () => {};
+  return (
+    <form onSubmit={handleSubmit}>
+      <ReCAPTCHA
+        ref={recaptchaRef}
+        size="normal"
+        sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SECRET}
+        onChange={onReCAPTCHAChange}
+      />
+      <input
+        onChange={handleChange}
+        required
+        type="email"
+        name="email"
+        placeholder="Email"
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
 }
